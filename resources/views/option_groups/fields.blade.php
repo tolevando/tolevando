68424eeda1727c39c9f5e4a2eeac5170459acafd
Everton Almeida <everton-cristian@hotmail.com>
2021-08-06 10:39:47 -0300

    #03 - criação de grupo de opção por estabelecimento, cadastro de grupo de opção unica e obrigatoria

--- resources/views/option_groups/fields.blade.php
+++ resources/views/option_groups/fields.blade.php
@@ -1,20 +1,45 @@
 @if($customFields)
 <h5 class="col-12 pb-4">{!! trans('lang.main_fields') !!}</h5>
 @endif
-<div style="flex: 50%;max-width: 50%;padding: 0 4px;" class="column">
+<div style="padding: 0 4px;" class=" row">
 <!-- Name Field -->
-<div class="form-group row ">
-  {!! Form::label('name', trans("lang.option_group_name"), ['class' => 'col-3 control-label text-right']) !!}
-  <div class="col-9">
-    {!! Form::text('name', null,  ['class' => 'form-control','placeholder'=>  trans("lang.option_group_name_placeholder")]) !!}
-    <div class="form-text text-muted">
-      {{ trans("lang.option_group_name_help") }}
+  <div class="form-group row col-5">
+    {!! Form::label('name', trans("lang.option_group_name"), ['class' => 'col-3 control-label text-right']) !!}
+    <div class="col-9">
+      {!! Form::text('name', null,  ['class' => 'form-control','placeholder'=>  trans("lang.option_group_name_placeholder")]) !!}
+      <div class="form-text text-muted">
+        {{ trans("lang.option_group_name_help") }}
+      </div>
     </div>
   </div>
+
+  <div class="form-group row col-4">
+    {!! Form::label('is_required', 'Opção Obrigatória',['class' => 'col-9 control-label text-right']) !!}
+      <div class="checkbox icheck">
+          <label class="col-9 ml-2 form-check-inline">
+              {!! Form::hidden('is_required', 0) !!}
+              {!! Form::checkbox('is_required', 1, null) !!}
+          </label>
+      </div>
+  </div>
+
+  {!! Form::hidden('market_id', (count(Auth::user()->markets) ? Auth::user()->markets->first()->id : null)) !!}
+
+  <div class="form-group row col-3">
+    {!! Form::label('is_unique', 'Opção Única',['class' => 'col-8 control-label text-right']) !!}
+      <div class="checkbox icheck">
+          <label class="col-9 ml-2 form-check-inline">
+              {!! Form::hidden('is_unique', 0) !!}
+              {!! Form::checkbox('is_unique', 1, null) !!}
+          </label>
+      </div>
+      <div class="form-text text-muted">
+        Limita a escolha de apenas uma opção no produto.
+      </div>
+  </div>
 </div>
-</div>
-<div style="flex: 50%;max-width: 50%;padding: 0 4px;" class="column">
-</div>
+
+
 @if($customFields)
 <div class="clearfix"></div>
 <div class="col-12 custom-field-container">
