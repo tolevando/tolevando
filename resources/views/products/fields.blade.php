2eaa282cfd26858c5c72a1a4760060a242f04a95
Everton Almeida <everton-cristian@hotmail.com>
2021-10-13 14:01:56 -0300

    #42 - inclusão de validação para pizza 2 sabores

--- resources/views/products/fields.blade.php
+++ resources/views/products/fields.blade.php
@@ -184,7 +184,7 @@
     <div class="form-group row " id="option_mid_pizza_label" style="display: none;">
         {!! Form::label('option_mid_pizza', 'Opção de meia pizza',['class' => 'col-3 control-label text-right']) !!}
         <div class="col-9">
-            {!! Form::select('option_mid_pizza', ['Não oferecer', 'Valor Médio', 'Valor Maior'], null, ['class' => 'select2 form-control', 'id="option_mid_pizza_value"']) !!}
+            {!! Form::select('option_mid_pizza', [1 => 'Valor Médio', 2 => 'Valor Maior'], null, ['class' => 'select2 form-control', 'id="option_mid_pizza_value"']) !!}
             <div class="form-text text-muted">Maior Valor (o valor será o maior entre as duas metades escolhidas)</div>
             <div class="form-text text-muted">Valor Médio (o valor será o valor médio entre das duas metades escolhidas)</div>
         </div>
